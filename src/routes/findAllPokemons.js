1b34873c0a7c224b5fed78338b322ee201955480
Hamza BEDOUI <hamza.bedoui@ausy.fr>
2022-06-23 01:06:47 +0200

    fix

--- src/routes/findAllPokemons.js
+++ src/routes/findAllPokemons.js
@@ -1,6 +1,6 @@
 const { Pokemon } = require("../db/sequelize");
 const { Op } = require("sequelize");
-const auth = require("../auth/auth");
+// const auth = require("../auth/auth");
 
 const capitalize = (str) => str.charAt(0).toUpperCase() + str.substring(1);
 
--- src/routes/findPokemonByPK.js
+++ src/routes/findPokemonByPK.js
@@ -2,7 +2,7 @@ const { Pokemon } = require("../db/sequelize");
 const auth = require("../auth/auth");
 
 module.exports = (app) => {
-  app.get("/api/pokemons/:id", (req, res) => {
+  app.get("/api/pokemons/:id", auth, (req, res) => {
     Pokemon.findByPk(req.params.id)
       .then((pokemon) => {
         if (pokemon === null) {
