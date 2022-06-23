f3df60fe8f0e0eba1f223f9a3270cfa604319ba6
Hamza BEDOUI <hamza.bedoui@ausy.fr>
2022-06-23 15:14:01 +0200

    fix host maria db in aws ec2

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -9,6 +9,7 @@ let sequelize;
 if (process.env.NODE_ENV === "production") {
   sequelize = new Sequelize("pokedex", "root", "besmillah", {
     host: "ec2-13-37-220-97.eu-west-3.compute.amazonaws.com",
+    // host: "localhost",
     dialect: "mariadb",
     dialectOptions: {
       timezone: "Etc/GMT-2",
