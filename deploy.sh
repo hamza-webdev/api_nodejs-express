c10b4955442636b2177c148fdd5901075a8290a6
Hamza BEDOUI <hamza.bedoui@ausy.fr>
2022-06-23 11:51:11 +0200

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 ssh -t -t -i "awskey.pem" ec2-user@ec2-13-37-220-97.eu-west-3.compute.amazonaws.com<<EOF
+   print("==== in script bash ====")
    cd api_nodejs-express
    git pull origin main
    npm install
